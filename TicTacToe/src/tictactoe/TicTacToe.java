4d46b75c8c4fa076ab22dca14502a2efe13a6baf
Pirjo43452 <pirjo43452@gmail.com>
2013-05-19 23:18:09 +0300

    Dokumentointikansio, luokkakaavio, tuntikirjanpito ja TicTacToen luokkia lisätty

--- TicTacToe/src/tictactoe/TicTacToe.java
+++ TicTacToe/src/tictactoe/TicTacToe.java
@@ -14,6 +14,6 @@ public class TicTacToe {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Game ticTacToe = new Game();
     }
 }
